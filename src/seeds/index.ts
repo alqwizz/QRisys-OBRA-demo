cb9ec0377b0be66033f82047df4d03fc7e5026fc
alqwizz <68905886+alqwizz@users.noreply.github.com>
2025-08-04 14:17:51 +0200

    Update index.ts

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -5,7 +5,7 @@ import rolSeed from './rol.seeds'
 require('dotenv').config();
 seeds();
 async function seeds() {
-    const uri = "mongodb+srv://infoqrisys:NB55NpdAe7AtTAa@qrisys.oxead.mongodb.net/QRiSYS?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://infoqrisys:NB55NpdAe7AtTAa@";
     await mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
     // Descomentar para borrar todos los roles y crear los roles por defecto
     await rolSeed();
